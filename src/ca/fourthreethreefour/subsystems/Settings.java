2994bf2379c2d2942a3f2df4f2f006864f1ef14a
Trevor Tsang <trevorkhtsang@gmail.com>
2018-01-14 14:09:30 -0700

    Super legit TalonSRX implementation that definitely isn't just copy/pasted from ATALibJ

--- src/ca/fourthreethreefour/subsystems/Settings.java
+++ src/ca/fourthreethreefour/subsystems/Settings.java
@@ -13,6 +13,12 @@ public interface Settings {
 	
 	//TODO ports
 	//int EXAMPLE_PORT = settingsFile.getIntProperty("EXAMPLE_PORT", [default port])
+	int DRIVE_LEFT_1 = settingsFile.getIntProperty("DRIVE_LEFT_1", 1);
+	int DRIVE_LEFT_2 = settingsFile.getIntProperty("DRIVE_LEFT_2", 2);
+	int DRIVE_LEFT_3 = settingsFile.getIntProperty("DRIVE_LEFT_3", 3);
+	int DRIVE_RIGHT_1 = settingsFile.getIntProperty("DRIVE_RIGHT_1", 4);
+	int DRIVE_RIGHT_2 = settingsFile.getIntProperty("DRIVE_RIGHT_2", 5);
+	int DRIVE_RIGHT_3 = settingsFile.getIntProperty("DRIVE_RIGHT_3", 6);
 	int GRAB_SOLENOID_1 = settingsFile.getIntProperty("GRAB_SOLENOID_1", 0);
 	int GRAB_SOLENOID_2 = settingsFile.getIntProperty("GRAB_SOLENOID_2", 1);
 	int MOTOR_SOLENOID_1 = settingsFile.getIntProperty("MOTOR_SOLENOID_1", 2);
