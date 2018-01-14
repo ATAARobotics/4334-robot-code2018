95f125d1e375b4e3b44898413d6c37f01fa35974
Aidan Paradis <acjparadis@outlook.com>
2018-01-13 19:54:11 -0700

    Added arm solenoid

--- src/ca/fourthreethreefour/subsystems/Arm.java
+++ src/ca/fourthreethreefour/subsystems/Arm.java
@@ -11,9 +11,10 @@ public interface Arm {
 	DualActionSolenoidModule 
 		wristSolenoid = new DualActionSolenoidModule(0, 1),
 		grabSolenoid = new DualActionSolenoidModule(2, 3),
-		motorSolenoid = new DualActionSolenoidModule(4, 5);
+		motorSolenoid = new DualActionSolenoidModule(4, 5),
+		armSolenoid = new DualActionSolenoidModule(6, 7);
 	
 	//Creates subsystem of above for use in Robot.java
-	Subsystem arm = new Subsystem(new Module[] { wristSolenoid, grabSolenoid, motorSolenoid });
+	Subsystem arm = new Subsystem(new Module[] { wristSolenoid, grabSolenoid, motorSolenoid, armSolenoid });
 
 }
