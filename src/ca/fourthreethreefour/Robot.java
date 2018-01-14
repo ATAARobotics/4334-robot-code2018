685c0168f156fa0d9bcd5b075d0a6acd4aad474d
Trevor Tsang <trevorkhtsang@gmail.com>
2018-01-13 19:09:08 -0700

    Added (mostly) blank methods from IterativeRobotAdapter

--- src/ca/fourthreethreefour/Robot.java
+++ src/ca/fourthreethreefour/Robot.java
@@ -8,7 +8,7 @@ public class Robot extends IterativeRobotAdapter {
 	
 	//Creates Subsystems AUTO and TELEOP that contains the code for whatever in the {}'s
 	private final Subsystem AUTO_MODULES = new Subsystem(
-			new Module[] { });
+			new Module[] { arm, drive });
 	
 	private final Subsystem TELEOP_MODULES = new Subsystem(
 			new Module[] { arm, drive });
@@ -16,9 +16,34 @@ public class Robot extends IterativeRobotAdapter {
 	//Puts the above two subsystems into this subsystem. Subsystemception
 	private final Subsystem ALL_MODULES =  new Subsystem(new Module[] { AUTO_MODULES, TELEOP_MODULES });
 	
-	//Constructor for the custom Robot class. Needed so GamePeriods accepts this class, or something.
+	/*	Constructor for the custom Robot class. Needed because IterativeRobotAdapter 
+		needs a name for some reason. */
 	public Robot() {
 		super("ATA 2018");
 	}
 	
+	@Override
+	public void init() {
+		ALL_MODULES.init();
+	}
+	
+	@Override
+	public void periodicDisabled() {
+		//TODO Robot should check settings file here
+	}
+	
+	@Override
+	public void initAutonomous() {
+		AUTO_MODULES.enable();
+	}
+	
+	@Override
+	public void initTeleoperated() {
+		TELEOP_MODULES.enable();
+	}
+	
+	@Override
+	public void periodicTeleoperated() {
+		
+	}
 }
