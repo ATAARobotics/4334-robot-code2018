4ff89e5e522cd22041e64bb7b8fb3736f2c57bfa
Trevor Tsang <trevorkhtsang@gmail.com>
2018-01-19 19:32:25 -0700

    C O M M E N T S

--- src/ca/fourthreethreefour/Robot.java
+++ src/ca/fourthreethreefour/Robot.java
@@ -46,15 +46,14 @@ public class Robot extends IterativeRobotAdapter {
 	public void init() {
 		// Initalizes all modules
 		ALL_MODULES.init();
-		//TODO add controls
 		
 		// Controller 1/driver
-		
-		// set minimum distance from middle
+		/*
+		 *  Sets the deadband for LEFT_FROM_MIDDLE and RIGHT_X. If the input value from either of those 
+		 *  axes does not exceed the deadband, the value will be set to zero.
+		 */
 		controller1.addDeadband(XboxController.LEFT_FROM_MIDDLE, 0.20);
 		controller1.changeAxis(XboxController.LEFT_FROM_MIDDLE, speedFunction);
-		
-		// set minimum distance from middle
 		controller1.addDeadband(XboxController.RIGHT_X, 0.20);
 		controller1.invertAxis(XboxController.RIGHT_X);
 		
@@ -71,41 +70,49 @@ public class Robot extends IterativeRobotAdapter {
             
 		});
 		
-		// When pressed X, run the arcadeDrive with speed RAMP_RELEASE_SPEED
-		controller1.addWhilePressed(XboxController.X, new Command() { //I have no clue how this works
+		/* 
+		 *  While X is pressed, run the rampTrain with speed RAMP_RELEASE_SPEED and no value for rotation.
+		 *
+		 *  Creates an anonymous class of type Command and specifies what it does when the run() function
+		 *  is called. Cool got confused about this.
+		 */
+		controller1.addWhilePressed(XboxController.X, new Command() {
 			@Override
 			public void run() {
-				rampTrain.arcadeDrive(RAMP_RELEASE_SPEED, 0); //takes the RAMP_RELEASE_SPEED as the speed for motor
+				//takes RAMP_RELEASE_SPEED, specified in settings.txt, as the speed for motor
+				rampTrain.arcadeDrive(RAMP_RELEASE_SPEED, 0); 
 				}
 			});
 		
-		// When pressed Y, Run the arcadeDrive with speed RAMP_RETRACT_SPEED
-		controller1.addWhilePressed(XboxController.Y, new Command() { //I have no clue how this works either
+		/* 
+		 *  While Y is pressed, run the rampTrain using arcadeDrive with speed RAMP_RETRACT_SPEED and no 
+		 *  value for rotation.
+		 */
+		controller1.addWhilePressed(XboxController.Y, new Command() {
 			@Override
 			public void run() {
-				rampTrain.arcadeDrive(RAMP_RETRACT_SPEED, 0); //takes the RAMP_RETRACT_SPEED as the speed for motor
+				//takes RAMP_RETRACT_SPEED, specified in settings.txt, as the speed for motor 
+				rampTrain.arcadeDrive(RAMP_RETRACT_SPEED, 0); 
 				}
 			});
-        //TODO add ramp controls
-		
+        
 		// Controller 2/operator
-		
 		// When left bumper is pressed, it reverses the grabSolenoid
 		controller2.addWhenPressed(XboxController.LEFT_BUMPER, new ReverseSolenoid(grabSolenoid));
 		
 		// When right bumper is pressed, it reverses the armSolenoid
 		controller2.addWhenPressed(XboxController.RIGHT_BUMPER, new ReverseSolenoid(armSolenoid));
 		
-		// When button pressed, set the solenoid state as left or right
-		controller2.addWhenPressed(XboxController.A, new SolenoidLeft(motorSolenoid)); //When pressed A, changes the solenoid to left
-		controller2.addWhenPressed(XboxController.B, new SolenoidRight(motorSolenoid)); //When pressed B, changed the solenoid to right
+		//When the A button is pressed, changes the solenoid state to left
+		//When the B button is pressed, changes the solenoid state to right
+		controller2.addWhenPressed(XboxController.A, new SolenoidLeft(motorSolenoid)); 
+		controller2.addWhenPressed(XboxController.B, new SolenoidRight(motorSolenoid)); 
 
 		// Sets a deadband to prevent input less than 0.1
 		controller2.addDeadband(XboxController.LEFT_TRIGGER, 0.1);
 		controller2.addDeadband(XboxController.RIGHT_TRIGGER, 0.1);
 		// Inverts the axis of the left_trigger
 		controller2.invertAxis(XboxController.LEFT_TRIGGER);
-		
 		// Binds the axis to the motor
 		controller2.addAxisBind(XboxController.LEFT_TRIGGER, armMotor);
 		controller2.addAxisBind(XboxController.RIGHT_TRIGGER, armMotor); 
@@ -117,42 +124,54 @@ public class Robot extends IterativeRobotAdapter {
 		
     	}
 	
+	// Runs at the beginning of autonomous
 	@Override
-	public void initAutonomous() { // when Autonomous is initialized
-		AUTO_MODULES.enable(); // Activate all auto_modules
-		drivetrain.setSafetyEnabled(false);// WE DONT NEED SAFETY
+	public void initAutonomous() { 
+		AUTO_MODULES.enable();
+		drivetrain.setSafetyEnabled(false); // WE DONT NEED SAFETY
 	}
 
-    	@Override
-    	public void endAutonomous() { // when Autonomous is ended
-    		drivetrain.setSafetyEnabled(true); // Maybe we do...
-    		AUTO_MODULES.disable(); // Disabled auto_modules
+	// Runs at the end of autonomous
+    @Override
+    public void endAutonomous() {
+    	AUTO_MODULES.disable();
    	 }
 	
+    // Runs at the beginning of teleop
 	@Override
-	public void initTeleoperated() { // when Teleoperated in initalized
-		TELEOP_MODULES.enable(); // Enable all teleop_modules
-		if (grabSolenoid.get() == Direction.OFF) { // if it off, set it right
+	public void initTeleoperated() { 
+		TELEOP_MODULES.enable();
+    	drivetrain.setSafetyEnabled(true); // Maybe we do...
+		/* 
+		 * If any of these solenoids are are in the OFF position, set them to a default position.
+		 * Necessary because most of our code for the solenoids reverses them, which cannot be done
+		 * for solenoids in the OFF position.
+		 * TODO check the default positions for these
+		 */
+		if (grabSolenoid.get() == Direction.OFF) {
 			grabSolenoid.set(DualActionSolenoid.Direction.RIGHT);
 		}
-		if (armSolenoid.get() == Direction.OFF) { // if it off, set it right
+		if (armSolenoid.get() == Direction.OFF) {
 			armSolenoid.set(DualActionSolenoid.Direction.RIGHT);
 		}
-		if (motorSolenoid.get() == Direction.OFF) { // if it off, set it left
+		if (motorSolenoid.get() == Direction.OFF) {
 			motorSolenoid.set(DualActionSolenoid.Direction.LEFT);
 		}
 	}
 	
+	// Runs every (approx.) 20ms in teleop
 	@Override
-	public void periodicTeleoperated() { // runs periodically during Teleoperated
-		controller1.doBinds();  // Performs every bind
+	public void periodicTeleoperated() {
+		// Performs the binds set in init()
+		controller1.doBinds();  
 		controller2.doBinds();
 	}
 	
+	// Runs at the end of teleop
 	@Override
-    	public void endTeleoperated() { // when Teleoperated is disabled
-        	TELEOP_MODULES.disable(); // disable teleop_modules
-    	}
+    public void endTeleoperated() {
+    	TELEOP_MODULES.disable();
+   	}
 
 }
 
