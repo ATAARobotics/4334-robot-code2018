3d7db4607062db7c397d03167cb6ad5e3f88b432
Kulkinz <cool@coolspages.com>
2018-03-28 16:38:26 -0600

    Changed controls

--- src/main/java/ca/fourthreethreefour/Robot.java
+++ src/main/java/ca/fourthreethreefour/Robot.java
@@ -99,15 +99,15 @@ public class Robot extends IterativeRobotAdapter implements Constants {
 		controller2.changeAxis(XboxController.TRIGGERS, armFunction);
 
 		// Binds the Intake motors to the Right stick
-		controller2.addDeadband(XboxController.RIGHT_FROM_MIDDLE, 0.12);
-		controller2.changeAxis(XboxController.RIGHT_FROM_MIDDLE, speedFunction);
-		controller2.invertAxis(XboxController.RIGHT_FROM_MIDDLE);
-		controller2.addAxisBind(controller2.getRightDistanceFromMiddle(), leftIntake);
+		controller1.addDeadband(XboxController.TRIGGERS, 0.12);
+		controller1.changeAxis(XboxController.TRIGGERS, speedFunction);
+		controller1.invertAxis(XboxController.TRIGGERS);
+		controller1.addAxisBind(controller1.getTriggers(), rightIntake);
 		//controller2.addAxisBind(controller2.getRightDistanceFromMiddle(), rightIntake);
-		controller2.addAxisBind(controller2.getRightDistanceFromMiddle(), new Output() {
+		controller1.addAxisBind(controller1.getTriggers(), new Output() {
 			@Override
 			public void set(double value) {
-				rightIntake.set(-value);
+				leftIntake.set(-value);
 			}
 		});
 		controller2.addDeadband(XboxController.LEFT_FROM_MIDDLE, 0.12);
@@ -292,10 +292,10 @@ public class Robot extends IterativeRobotAdapter implements Constants {
 					}
 				} else {
 					if (gameData.charAt(0) == 'R') { // if our side of the switch is on the right
--						Commands.run(commandQualsRight);
--					} else {
--						Commands.run(commandQualsLeft);
--					}
+						Commands.run(commandQualsRight);
+					} else {
+						Commands.run(commandQualsLeft);
+					}
 				}
 			}
 		}
