dbc278e333314c7b3cf262d1110bf0d4c02717ea
Jacob-Guglielmin <jacobguglielmin@gmail.com>
2022-07-26 18:29:43 -0600

    update

--- src/main/java/frc/robot/subsystems/LightingSubsystem.java
+++ src/main/java/frc/robot/subsystems/LightingSubsystem.java
@@ -15,7 +15,7 @@ public class LightingSubsystem extends SubsystemBase {
     private int moveLightCount = 0;
     private int offset = 0;
     private int[][] colorList;
-    
+
     public LightingSubsystem() {
         candle = new CANdle(2);
         config = new CANdleConfiguration();
@@ -23,13 +23,13 @@ public class LightingSubsystem extends SubsystemBase {
         config.brightnessScalar = 1;
         candle.configAllSettings(config);
 
-        generateGradient(5, new int[] {255, 0, 0}, new int[] {0, 255, 0}, new int[] {0, 0, 255});
+        colorList = generateGradient(5, new int[] { 255, 0, 0 }, new int[] { 0, 255, 0 }, new int[] { 0, 0, 255 });
 
         candle.setLEDs(0, 0, 0);
     }
 
-    private void generateGradient(int step, int[]... colors) {
-        colorList = new int[3][step * colors.length];
+    private int[][] generateGradient(int step, int[]... colors) {
+        int[][] newColorList = new int[3][step * colors.length];
         for (int color = 0; color < colors.length; color++) {
             int[] thisColor = colors[color];
             int[] nextColor;
@@ -38,12 +38,14 @@ public class LightingSubsystem extends SubsystemBase {
             } else {
                 nextColor = colors[0];
             }
-            int[] distance = {nextColor[0] - thisColor[0], nextColor[1] - thisColor[1], nextColor[2] - thisColor[2]};
+            int[] distance = { nextColor[0] - thisColor[0], nextColor[1] - thisColor[1], nextColor[2] - thisColor[2] };
 
             for (int i = 0; i < step; i++) {
-                colorList[color + i] = new int[] {(int) distance[0] * (i / step), (int) distance[1] * (i / step), (int) distance[2] * (i / step)};
+                newColorList[color + i] = new int[] { (int) distance[0] * (i / step), (int) distance[1] * (i / step),
+                        (int) distance[2] * (i / step) };
             }
         }
+        return newColorList;
     }
 
     public void lightUp(double lightPercent) {
@@ -54,14 +56,20 @@ public class LightingSubsystem extends SubsystemBase {
             offset %= colorList.length;
         }
 
-        int count = (int) Math.floor(50*lightPercent);
-        candle.setLEDs(0, 0, 0, 0, 8+count, 50-count);
-        candle.setLEDs(0, 0, 0, 0, 78, 50-count);
-        // candle.setLEDs(0, (int) Math.round(230*lightPercent)+25, (int) Math.round(230*(-lightPercent+1))+25, 0, 8, count);
-        // candle.setLEDs(0, (int) Math.round(230*lightPercent)+25, (int) Math.round(230*(-lightPercent+1))+25, 0, 128-count, count);
-        for (int i = 0; i < count-2; i++) {
-            candle.setLEDs(colorList[(i-offset+colorList.length) % colorList.length][0], colorList[(i-offset+colorList.length) % colorList.length][1], colorList[(i-offset+colorList.length) % colorList.length][2], 0, i+8, 1);
-            candle.setLEDs(colorList[(i-offset+colorList.length) % colorList.length][0], colorList[(i-offset+colorList.length) % colorList.length][1], colorList[(i-offset+colorList.length) % colorList.length][2], 0, 127-i, 1);
+        int count = (int) Math.floor(50 * lightPercent);
+        candle.setLEDs(0, 0, 0, 0, 8 + count, 50 - count);
+        candle.setLEDs(0, 0, 0, 0, 78, 50 - count);
+        // candle.setLEDs(0, (int) Math.round(230*lightPercent)+25, (int)
+        // Math.round(230*(-lightPercent+1))+25, 0, 8, count);
+        // candle.setLEDs(0, (int) Math.round(230*lightPercent)+25, (int)
+        // Math.round(230*(-lightPercent+1))+25, 0, 128-count, count);
+        for (int i = 0; i < count - 2; i++) {
+            candle.setLEDs(colorList[(i - offset + colorList.length) % colorList.length][0],
+                    colorList[(i - offset + colorList.length) % colorList.length][1],
+                    colorList[(i - offset + colorList.length) % colorList.length][2], 0, i + 8, 1);
+            candle.setLEDs(colorList[(i - offset + colorList.length) % colorList.length][0],
+                    colorList[(i - offset + colorList.length) % colorList.length][1],
+                    colorList[(i - offset + colorList.length) % colorList.length][2], 0, 127 - i, 1);
         }
     }
 }
